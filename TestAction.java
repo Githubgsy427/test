b9f91fd74aaec5aebee46f0e43ba3a5303db64e0
Githubgsy427 <775315917@qq.com>
2021-07-04 01:28:36 +0800

    测试1

--- TestAction.java
+++ TestAction.java
@@ -2,6 +2,6 @@ public class TestAction
 {
 	public static void main(String[] args) {
 		System.out.println("[java] java code run normally.");
-		System.out.println("我其实是爱你的呀");
+		System.out.println("我其实是爱你的呀")
        }
 }
